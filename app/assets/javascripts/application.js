5312f9f7b071133fd797c6748e30dd4e0e06093f
Chuck Lauer Vose <vosechu@gmail.com>
2013-07-29 18:12:48 -0700

    get erica's spec index.html file running in /public

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,68 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+// Some general UI pack related JS
+// Extend JS String with repeat method
+String.prototype.repeat = function(num) {
+    return new Array(num + 1).join(this);
+};
+
+(function($) {
+
+  // Add segments to a slider
+  $.fn.addSliderSegments = function (amount) {
+    return this.each(function () {
+      var segmentGap = 100 / (amount - 1) + "%"
+        , segment = "<div class='ui-slider-segment' style='margin-left: " + segmentGap + ";'></div>";
+      $(this).prepend(segment.repeat(amount - 2));
+    });
+  };
+
+  $(function() {
+  
+    // Todo list
+    $(".todo li").click(function() {
+        $(this).toggleClass("todo-done");
+    });
+
+    // Custom Select
+    $("select[name='herolist']").selectpicker({style: 'btn-primary', menuStyle: 'dropdown-inverse'});
+
+    // Tooltips
+    $("[data-toggle=tooltip]").tooltip("show");
+
+    // Tags Input
+    $(".tagsinput").tagsInput();
+
+    // jQuery UI Sliders
+    var $slider = $("#slider");
+    if ($slider.length) {
+      $slider.slider({
+        min: 1,
+        max: 5,
+        value: 2,
+        orientation: "horizontal",
+        range: "min"
+      }).addSliderSegments($slider.slider("option").max);
+    }
+
+    // Placeholders for input/textarea
+    $("input, textarea").placeholder();
+
+    // Make pagination demo work
+    $(".pagination a").on('click', function() {
+      $(this).parent().siblings("li").removeClass("active").end().addClass("active");
+    });
+
+    $(".btn-group a").on('click', function() {
+      $(this).siblings().removeClass("active").end().addClass("active");
+    });
+
+    // Disable link clicks to prevent page scrolling
+    $('a[href="#fakelink"]').on('click', function (e) {
+      e.preventDefault();
+    });
+
+    // Switch
+    $("[data-toggle='switch']").wrap('<div class="switch" />').parent().bootstrapSwitch();
+    
+  });
+  
+})(jQuery);
