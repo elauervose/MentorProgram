6953f2ce26a565aa5e9e6ee83a17e1ad441b4275
Nick Reich <nreich@gmail.com>
2013-08-12 15:54:42 -0700

    Add presence validation to category name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,6 +5,7 @@ describe Category do
 
   it { should respond_to :name }
   it { should respond_to :asks }
+  it { should be_valid }
 
   describe "asks" do
     let(:ask) { FactoryGirl.create :ask }
@@ -12,6 +13,13 @@ describe Category do
     it "can be added to an ask" do
       ask.categories << category
       expect(ask.categories).to include(category)
+    end
+  end
+
+  describe "validations" do
+    it "should require a name" do
+      category.name = ""
+      expect(category).to_not be_valid
     end
   end
 end
