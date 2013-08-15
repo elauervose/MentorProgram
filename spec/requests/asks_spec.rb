69a3545186dd6160eb4299c4ccbcf8c6300bdee7
Nick Reich <nreich@gmail.com>
2013-08-14 19:17:43 -0700

    Create ask#show page

--- spec/requests/asks_spec.rb
+++ spec/requests/asks_spec.rb
@@ -105,4 +105,23 @@ describe "Asks" do
    end
   end
 
+  describe "show page" do
+    let(:ask) { FactoryGirl.create :ask }
+
+    before { visit ask_path(ask) }
+
+    it "should have information about the mentee request" do
+      expect(page).to have_selector 'p', text: ask.name
+    end
+    it "should have a link to become their mentor" do
+      expect(page).to have_link 'Become Mentor!',
+        href: "/answers/new?ask_id=#{ask.id}"     
+    end
+    it "should have a link to mentors sign up page" do
+      expect(page).to have_link "Return to Mentees List",
+        href: mentors_sign_up_path
+    end
+  end
+
+
 end
