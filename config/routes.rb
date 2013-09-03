34b015b80052cadff5935775f05c6c27728f45ea
Nick Reich <nreich@gmail.com>
2013-09-03 14:58:00 -0700

    Clean up partials for showing table to asks to reflect move into asks#index action

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Mentor::Application.routes.draw do
   get "mentees", to: 'asks#new'
   get "prep", to: 'static#prep'
   get "resources", to: 'static#resources'
-  get "asks/new", to: 'asks#new'
   get "thank_you_mentor", to: 'static#thank_you_mentor'
   get "thank_you_mentee", to: 'static#thank_you_mentee'
   # The priority is based upon order of creation: first created -> highest priority.
