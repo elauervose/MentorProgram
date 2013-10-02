e10f2170d0a930b316ed700edbb3f91379156aba
Nick Reich <nreich@gmail.com>
2013-10-02 15:58:22 -0700

    Add count of requests and answered requests to locations#statistics page

--- spec/requests/locations/statistics_spec.rb
+++ spec/requests/locations/statistics_spec.rb
@@ -21,4 +21,18 @@ describe "Location statistics" do
       expect(page).to have_text "#{location.average_response_time.to_i} days"
     end
   end
+  describe "number of requests" do
+    specify "total should be present" do
+      ask = FactoryGirl.create(:mentor_ask, locations: [location])
+      ask.create_answer(FactoryGirl.attributes_for(:answer))
+      visit statistics_locations_path
+      expect(page).to have_text("1 request")
+    end
+    specify "answered total should be present" do
+      ask = FactoryGirl.create(:mentor_ask, locations: [location])
+      ask.create_answer(FactoryGirl.attributes_for(:answer))
+      visit statistics_locations_path
+      expect(page).to have_text("1 request answered")
+    end
+  end
 end
