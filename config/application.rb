bfbb72a0c128912a4f39359be9b9353ae880a517
Nick Reich <nreich@gmail.com>
2013-10-03 10:04:02 -0700

    Use Pry for rails console

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,14 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+
 module Mentor
   class Application < Rails::Application
+
+    console do
+      require "pry"
+      config.console = Pry
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
