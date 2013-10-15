09c8cbcb433293e2d76425d7495dd3fbe822b75a
Nick Reich <nreich@gmail.com>
2013-10-14 17:10:52 -0700

    Modify secret token config to use heroku config var in production and use a simple (unsecure) string in test and development environments.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Mentor::Application.config.secret_key_base = 'cccc1cc8b1d8f21246c5f6dce99e6cf7763a8930f7be3e54fda5f638af05854b4ceb5e09e5337b0e0499962ad5167b01ab93b998e5652aa515d62e1b61430efa'
+if Rails.env.development? || Rails.env.test?
+  Mentor::Application.config.secret_key_base = 'a' * 30
+else
+  ENV['SECRET_TOKEN']
+end
