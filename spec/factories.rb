2271deb4688b1ccd301d7fe6cc2ae6fdfb9401c3
Nick Reich <nreich@gmail.com>
2013-10-31 11:37:41 -0700

    By default, set asks to be validated when created by FactoryGirl

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,6 +6,7 @@ FactoryGirl.define do
       to tech to find mentors interested in helping them go through
       three 1-2 hour sessions and help them iwth their problems"
     email_updates true
+    validated_at Time.now
     locations {[FactoryGirl.create(:location)]}
     meetup_times {[FactoryGirl.create(:meetup_time)]}
   end
