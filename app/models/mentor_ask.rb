beda5c1ac115aee64da858f6b9443e54decc46f2
Nick Reich <nreich@gmail.com>
2013-10-31 15:15:28 -0700

    Add after_create actions within MentorAsk model so they run after creation of categories through accepts_nested_attributes_for. Otherwise, category is created but assosciation is not formed with the MentorAsk

--- app/models/mentor_ask.rb
+++ app/models/mentor_ask.rb
@@ -2,6 +2,7 @@ class MentorAsk < Ask
   has_and_belongs_to_many :categories, foreign_key: 'ask_id'
   accepts_nested_attributes_for :categories, reject_if:
     proc { |attributes| attributes['name'].blank? }
+  after_create :create_token, :send_validation_email
 
   scope :with_category, ->(category) do
     return all if category.blank?
