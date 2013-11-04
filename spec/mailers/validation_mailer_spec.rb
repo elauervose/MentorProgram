1b1f7b7498963b39c381258b18d5a60fc8648cb8
Nick Reich <nreich@gmail.com>
2013-11-04 14:12:56 -0800

    Add link to validation email to ask's show page with token supplied to allow deletion

--- spec/mailers/validation_mailer_spec.rb
+++ spec/mailers/validation_mailer_spec.rb
@@ -19,12 +19,31 @@ describe ValidationMailer do
       expect(validation_email.body).to have_link(
         validate_asks_path(token: ask.token))
     end
-
     it "should be sent successfully" do
       expect(ActionMailer::Base.deliveries).to_not be_empty
+    end
+
+  end
+
+  describe "deleting the request" do
+    context "for a mentor_ask" do
+      it "should contain a link to view and delete the request" do
+        mentor_ask = FactoryGirl.create(:mentor_ask, validated_at: nil)
+        validation_email = ValidationMailer.request_validation(mentor_ask).
+          deliver
+        expect(validation_email.body).to have_link("delete_request", href:
+          mentor_ask_path(mentor_ask, token: mentor_ask.token))
+      end
+    end
+    context "for a pair_ask" do
+      it "should contain a link to view and delete the request" do
+        pair_ask = FactoryGirl.create(:pair_ask, validated_at: nil)
+        validation_email = ValidationMailer.request_validation(pair_ask).
+          deliver
+        expect(validation_email.body).to have_link("delete_request", href:
+          pair_ask_path(pair_ask, token: pair_ask.token))
+      end
     end
   end
 
 end
-
-
